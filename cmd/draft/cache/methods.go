c3445f12d32ff0d3f536cab80592486652885fb3
Gleb <gleb.gleb.off1@gmail.com>
2025-08-29 21:53:11 +0300

    second version suggested
    what's next? container/heap?

---
+++ cmd/draft/cache/methods.go
@@ -0,0 +1,235 @@
+package cache
+
+import (
+	"errors"
+	"maps"
+	"math"
+	"reflect"
+	"sync/atomic"
+	"time"
+	"unsafe"
+)
+
+const elemsPerTable = 896 // it's used for memory reallocating
+
+func (c *inMemoryCache[K, V]) Add(key K, value V, ttl time.Duration) error {
+	if ttl < c.step {
+		return errors.New("ttl must be more than cache check step")
+	}
+	t := time.Now().Truncate(c.step)
+	ttl = ttl.Truncate(c.step)
+	t = t.Add(ttl)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.deleteKeyFromQueue(key)
+
+	c.cache[key] = Value[V]{time: t, value: value}
+	keys, ok := c.queue[t]
+	if !ok {
+		c.queue[t] = make([]K, 0, c.queueSize)
+	}
+	c.queue[t] = append(keys, key)
+	c.addTime(t)
+	c.resetTicker()
+
+	return nil
+}
+
+func (c *inMemoryCache[K, V]) Get(key K) (V, bool) {
+	c.mu.RLock()
+	value, ok := c.cache[key]
+	c.mu.RUnlock()
+	return value.value, ok
+}
+
+func (c *inMemoryCache[K, V]) Delete(keys ...K) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for _, key := range keys {
+		/*
+			the code is correct, but it takes too much CPU time
+			to find and delete something that will eventually be deleted
+				c.deleteKeyFromQueue(key)
+		*/
+		delete(c.cache, key)
+	}
+
+	// добавить срезы
+
+}
+
+func (c *inMemoryCache[K, V]) StartGC() {
+	if !c.isGC.CompareAndSwap(0, 1) {
+		return
+	}
+	go func() {
+		t := time.NewTicker(c.gcInterval)
+		cond := atomic.Int32{}
+		(&cond).Store(1)
+		for {
+			select {
+			case <-t.C:
+				switch c.isGC == cond {
+				case true:
+					c.realloc()
+				case false:
+					return
+				}
+			case <-c.closeChan:
+				return
+			}
+		}
+	}()
+}
+
+/*
+	func (c *inMemoryCache[K, V]) StopGC() {
+		c.isGC.CompareAndSwap(1, 0)
+	}
+*/
+func (c *inMemoryCache[K, V]) Close() {
+	close(c.closeChan)
+}
+
+func (c *inMemoryCache[K, V]) realloc() {
+	mapValue := reflect.ValueOf(c.cache)
+	mapPointer := unsafe.Pointer(mapValue.Pointer())
+	type Map struct {
+		used              uint64
+		seed              uintptr
+		dirPtr            unsafe.Pointer
+		dirLen            int
+		globalDepth       uint8
+		globalShift       uint8
+		writing           uint8
+		tombstonePossible bool
+		clearSeq          uint64
+	}
+
+	mapPtr := (*Map)(mapPointer)
+	tableCount := mapPtr.dirLen
+	elemCount := mapPtr.used // or just len(c.cache)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if int(math.Log2(float64(tableCount*elemsPerTable/int(elemCount)))) > 1 {
+		mapCopy := make(map[K]Value[V], elemCount)
+		maps.Copy(mapCopy, c.cache)
+		c.cache = mapCopy
+	}
+	timesCopy := make([]time.Time, len(c.times), cap(c.times))
+	copy(timesCopy, c.times)
+	c.times = timesCopy
+}
+
+func (c *inMemoryCache[K, V]) clean() {
+	go func() {
+		for {
+			select {
+			case <-c.closeChan:
+				return
+			case <-c.ticker.C:
+				c.mu.RLock()
+				keys, ok := c.queue[time.Now().Truncate(c.step)]
+				c.mu.RUnlock()
+				if !ok {
+					continue // or break
+				}
+				c.mu.Lock()
+				for _, key := range keys {
+					delete(c.cache, key)
+				}
+				c.times = c.times[1:]
+				c.resetTicker()
+				c.mu.Unlock()
+			}
+		}
+	}()
+}
+
+// not concurrent-safe
+func (c *inMemoryCache[K, V]) deleteKeyFromQueue(key K) {
+	v, ok := c.cache[key]
+	if !ok {
+		return
+	}
+	keySlice := c.queue[v.time]
+	for i, k := range keySlice {
+		if k == key {
+			keySlice[i] = keySlice[len(keySlice)-1]
+			c.queue[v.time] = keySlice[:len(keySlice)-1]
+			//keySlice=append(keySlice[:i], keySlice[i+1:]...)
+			break
+		}
+	}
+}
+
+func (c *inMemoryCache[K, V]) addTime(t time.Time) {
+	index, exists := c.findIndex(t)
+	if exists {
+		return
+	}
+	if len(c.times) == cap(c.times) {
+		c.times = append(c.times, time.Time{})
+	}
+	copy(c.times[index+1:], c.times[index:])
+	c.times[index] = t
+	if index == 0 {
+		c.resetTicker()
+	}
+}
+
+func (c *inMemoryCache[K, V]) resetTicker() {
+	c.ticker.Reset(c.times[0].Sub(time.Now().Truncate(c.step)))
+}
+
+// returns index before which you should place new value
+// 3 -> [0, 5, 9]: the function returns 1
+func (c *inMemoryCache[K, V]) findIndex(t time.Time) (index int, exists bool) {
+	l := len(c.times)
+	i, j := 0, l
+	for i < j {
+		mid := (i + j) >> 1
+		switch c.times[mid].Compare(t) {
+		case -1:
+			i = mid + 1
+		case 0:
+			return mid, true
+		case 1:
+			j = mid
+		}
+	}
+	return i, false
+}
+
+// Test block
+
+func findIndex(ts []time.Time, t time.Time) (index int, exists bool) {
+	l := len(ts)
+	i, j := 0, l
+	for i < j {
+		mid := (i + j) >> 1
+		switch ts[mid].Compare(t) {
+		case -1:
+			i = mid + 1
+		case 0:
+			return mid, true
+		case 1:
+			j = mid
+		}
+	}
+	return i, false
+}
+
+func addTime(ts []time.Time, t time.Time) []time.Time {
+	index, exists := findIndex(ts, t)
+	if exists {
+		return ts
+	}
+	if len(ts) == cap(ts) {
+		ts = append(ts, time.Time{})
+	}
+	copy(ts[index+1:], ts[index:])
+	ts[index] = t
+	return ts
+}
---
+++ cmd/draft/cache/setup.go
@@ -0,0 +1,99 @@
+package cache
+
+import (
+	"errors"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Value[V any] struct {
+	time  time.Time
+	value V
+}
+
+type inMemoryCache[K comparable, V any] struct {
+	mu         sync.RWMutex
+	cache      map[K]Value[V]    // map for storing keys and values
+	queue      map[time.Time][]K // map that's used for storing keys and their expiration time
+	step       time.Duration     // minimal time clock to check whether keys are expired
+	queueSize  int               // size of []K - depends on use case
+	isGC       atomic.Int32      // it's used to start goroutine that reallocates the memory
+	gcInterval time.Duration     // interval to check whether memory needs to be reallocated
+	closeChan  chan struct{}
+
+	times  []time.Time
+	ticker *time.Ticker
+}
+
+type options struct {
+	step       time.Duration
+	queueSize  int
+	gcInterval time.Duration
+}
+
+type Option func(options *options) error
+
+func WithStep(step time.Duration) Option {
+	return func(options *options) error {
+		if step < 0 {
+			return errors.New("step time must be positive")
+		}
+		options.step = step
+		return nil
+	}
+}
+
+func WithQueueSize(size int) Option {
+	return func(options *options) error {
+		if size < 0 {
+			return errors.New("queue size must be positive")
+		}
+		options.queueSize = size
+		return nil
+	}
+}
+
+func WithGCInterval(interval time.Duration) Option {
+	return func(options *options) error {
+		if interval < 0 {
+			return errors.New("GC interval must be positive")
+		}
+		options.gcInterval = interval
+		return nil
+	}
+}
+
+func New[K comparable, V any](opts ...Option) (*inMemoryCache[K, V], error) {
+	var options options
+	for _, opt := range opts {
+		err := opt(&options)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if options.step == 0 {
+		options.step = time.Second
+	}
+	if options.queueSize == 0 {
+		options.queueSize = 5
+	}
+	if options.gcInterval == 0 {
+		options.gcInterval = time.Minute
+	}
+
+	c := &inMemoryCache[K, V]{
+		cache:      make(map[K]Value[V]),
+		queue:      make(map[time.Time][]K),
+		step:       options.step,
+		queueSize:  options.queueSize,
+		gcInterval: options.gcInterval,
+		closeChan:  make(chan struct{}),
+		times:      make([]time.Time, 10),
+		ticker:     time.NewTicker(time.Hour),
+	}
+	c.clean()
+
+	return c, nil
+}
